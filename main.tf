619c7849758438b45c208b18823be90f9ca1ac38
Artem A <67011886+hi-artem@users.noreply.github.com>
2022-03-29 11:24:38 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 2.0"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
